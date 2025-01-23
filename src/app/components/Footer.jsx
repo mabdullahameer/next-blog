0354daf49ef90c459b3c2d72deda626333bb7cf6
mabdullahameer <itmabdullahameer@gmail.com>
2025-01-23 10:50:12 +0500

    push to here

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -20,9 +20,9 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Sahand&apos;s
+               Health Care 
               </span>
-              Blog
+              Center
             </Link>
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
@@ -71,7 +71,7 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Sahand's blog"
+            by="Health Care Center"
             year={new Date().getFullYear()}
           />
           <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
@@ -79,7 +79,7 @@ export default function FooterCom() {
             <Footer.Icon href='#' icon={BsInstagram} />
             <Footer.Icon href='#' icon={BsTwitter} />
             <Footer.Icon
-              href='https://github.com/sahandghavidel'
+              href='#'
               icon={BsGithub}
             />
             <Footer.Icon href='#' icon={BsDribbble} />
